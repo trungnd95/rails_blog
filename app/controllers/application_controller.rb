08a8ba1dc2d6b77d3d0288d4cf56b05a35da45e4
Trung Nguyen <ndt8895@gmail.com>
2016-10-04 04:07:24 +0700

    Add search functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_search
+
+  private
+  def set_search
+    @q = Post.ransack(params[:q])
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,13 @@ class PostsController < ApplicationController
 	before_action :find_post, only: [:show, :edit, :update, :destroy]
 	before_action :authenticate_user!, except: [:index, :show]
 	before_action :log_impression, :only=> [:show]
+	before_action :set_search
 
 	def index
 		if params[:tag]
 			@posts =  Post.tagged_with(params[:tag].strip).paginate(page: params[:page], per_page: 5)
+		elsif params[:q]
+	  	@posts = @q.result(distinct: true).paginate(page: params[:page], per_page: 5)
 		else
 			@posts = Post.all.order("created_at desc").paginate(page: params[:page], per_page: 5)
 		end
@@ -58,4 +61,6 @@ class PostsController < ApplicationController
 	  @post = Post.friendly.find(params[:id])
 	  @post.impressions.create(ip_address: request.remote_ip)
 	end
+
+
 end
