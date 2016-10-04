6b2ed2e794633dc07abf5c6b4b79089a6939d458
Trung Nguyen <ndt8895@gmail.com>
2016-10-04 10:15:12 +0700

    Precompile local environment

---
+++ public/assets/comments-b676226fb25d98591e5b81b0daf456f54afb9d6befa8dec4840cd06e1bdce636.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+  var comments =  $('.comments_number').html();
+  $('#new_comment').on('submit', function(e){
+    e.preventDefault();
+    data = $(this).serialize();
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: 'POST',
+      dataType: 'JSON',
+      data: data,
+      success: function(result) {
+        var comments =  $('.comments_number').html();
+        var comments_number =  parseInt(comments.split(' ')[0]);
+        comments_number += 1;
+        if(comments_number == 1) {
+          $('.comments_number').html(comments_number + ' comment');
+        }else {
+          $('.comments_number').html(comments_number + ' comments');
+        }
+
+        var html = '<div class="comment">';
+        html += '<p><strong>' + result.name +'</strong></p>';
+        html += '<p>' + result.content + '</p></div>';
+        $('.comments').append(html);
+
+      },
+      error: function(error) {
+        alert(error);
+      },
+      complete: function(){
+        $('#new_comment')[0].reset();
+      }
+
+    })
+  })
+})
+
+
+;
